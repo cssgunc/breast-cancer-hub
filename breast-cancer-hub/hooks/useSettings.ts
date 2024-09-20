a5509b664ff779e073b26e316e5534b907900f57
Matt <mzhu103@gmail.com>
2024-09-19 22:03:19 -0400

    typeguard and multideploy settings

---
+++ breast-cancer-hub/hooks/useSettings.ts
@@ -0,0 +1,44 @@
+import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
+type SettingsMap = {
+  "email": string,
+  "token": string,
+
+  "schedulingType": {day: number} | "period"
+
+  "notificationTimes": {hour: number, minute: number}[], //using expo-notifications trigger format
+
+  "locale": string, //using expo-localization
+
+  "useBackupData": boolean,
+  "useTelemetry": boolean,
+  "usePushNotifications": boolean,
+  "useInAppNotifications": boolean,
+
+}
+
+type SettingKeys = keyof SettingsMap;
+
+export async function getSetting<T extends SettingKeys>(key: T): Promise<SettingsMap[T] | null>{
+  let res
+  if (Platform.OS === 'web') {
+    res = await AsyncStorage.getItem(key)
+  }else{
+    res = await SecureStore.getItemAsync(key)
+  }
+  if(res){
+    return JSON.parse(res)
+  }
+  return null
+}
+
+
+export async function saveSetting<T extends SettingKeys>(key: T, value: SettingsMap[T]) {
+  if (Platform.OS === 'web') {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  }else{
+    await SecureStore.setItemAsync(key, JSON.stringify(value));
+  }
+
+}
