893197d6ae80f30ae8f97600ec39e8fb0a0a5974
Matt <mzhu103@gmail.com>
2024-10-05 22:14:38 -0400

    add backup settings middleware, shorten getSettings type, document settings globals

--- breast-cancer-hub/hooks/useSettings.ts
+++ breast-cancer-hub/hooks/useSettings.ts
@@ -16,12 +16,13 @@ export type SettingsMap = {
   "useTelemetry": boolean,
   "usePushNotifications": boolean,
   "useInAppNotifications": boolean,
-
 }
 
 export type SettingKeys = keyof SettingsMap;
 
-export async function getSetting<T extends SettingKeys>(key: T): Promise<SettingsMap[T] | null>{
+const OrderedSettingsKeys : SettingKeys[] = ["email", "token", "schedulingType", "notificationTimes", "locale", "useBackupData", "useTelemetry", "usePushNotifications", "useInAppNotifications"]
+
+export async function getSetting<T extends SettingKeys>(key: T): Promise<SettingsMap[T]>{
   let res
   if (Platform.OS === 'web') {
     res = await AsyncStorage.getItem(key)
@@ -31,7 +32,7 @@ export async function getSetting<T extends SettingKeys>(key: T): Promise<Setting
   if(res){
     return JSON.parse(res)
   }
-  return null
+  return generateDefaultSettings()[key]
 }
 
 
@@ -42,4 +43,47 @@ export async function saveSetting<T extends SettingKeys>(key: T, value: Settings
     await SecureStore.setItemAsync(key, JSON.stringify(value));
   }
 
+}
+
+export async function backupSettings(){
+  if (await getSetting("useBackupData") === true){
+    //TODO: add database specific code to push a backup
+  }
+}
+
+export async function loadBackupSettings(){
+  if (await getSetting("useBackupData") === true){
+    //TODO: add database specific code to load a backup
+  }
+}
+//meant to be used in backupSettings()
+export async function generateSettingsJson(){
+  //bit of a hacky solution for now
+  const settings : any = generateDefaultSettings()
+
+  for(const key of OrderedSettingsKeys){
+    const value = await getSetting(key)
+    if(value!=null){ //if we saved the setting override the default
+      settings[key] = value
+    }
+  }
+
+  return JSON.stringify(settings)
+}
+
+export function generateDefaultSettings(){
+  const def: SettingsMap = {
+    email: '',
+    token: '',
+    schedulingType: {
+      day: 0
+    },
+    notificationTimes: [],
+    locale: 'en-US',
+    useBackupData: false,
+    useTelemetry: false,
+    usePushNotifications: false,
+    useInAppNotifications: false
+  }
+  return def
 }
