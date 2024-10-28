b2aeb89e7f760130472d5f5ee373a48e30e226f8
Toren Kristoffersen <kristtor@unc.edu>
2024-10-28 19:15:26 -0400

    This component should be able to act as a button to navigate to the settings page. However, it is hardcoded in the pink colors

---
+++ breast-cancer-hub/components/navigation/SettingsIconButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function IconButton() {
+  return (
+    <Link href="/settings" asChild>
+      <Pressable style={styles.button}>
+        <MaterialIcons name="settings" size={24} color="#E93C92" />
+      </Pressable>
+    </Link>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,          
+    height: 50,
+    borderRadius: 25,   
+    backgroundColor: '#EFCEE6',  
+    alignItems: 'center',  
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+});
