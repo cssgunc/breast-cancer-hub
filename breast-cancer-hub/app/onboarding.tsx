8208431af42106c5eddee3ffae76db37dbf19014
Toren Kristoffersen <kristtor@unc.edu>
2025-03-02 21:29:45 -0500

    Added information (the purple text from the notes with Dr. Lopa) under onboarding.tsx, added functionality so when completed with onboarding you are redirected to the menstruation settings component, and also added some progress 'dots' to track what page the user is on.

--- breast-cancer-hub/app/onboarding.tsx
+++ breast-cancer-hub/app/onboarding.tsx
@@ -1,34 +1,388 @@
-import { StyleSheet } from "react-native";
-
+import React, { useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
+import { AccountSettingsHeaderComponent } from "@/components/AccountSettingsHeader";
+import { LearnMoreTextContainer } from "@/components/LearnMoreText";
+import { colors, globalStyles } from "@/components/StyleSheet";
+import StepIndicators from "@/components/StepIndicators";
+import { ExternalLink } from "@/components/ExternalLink";
+import { router } from "expo-router";
 
-export default function HomeScreen() {
+export default function OnboardingScreen() {
+  const [step, setStep] = useState(0);
+  const totalSteps = 4; 
+
+  const handleNext = () => {
+    if (step < totalSteps - 1) {
+      setStep(step + 1);
+    }
+  };
+
+  const handleBack = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+
   return (
-    <ThemedView style={styles.bodyContainer}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">A Commission for BreastCancerHub</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.popText}>
-        <ThemedText type="default">Tab for onboarding</ThemedText>
-      </ThemedView>
+    <ThemedView style={styles.container}>
+      <AccountSettingsHeaderComponent />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        {step === 0 && (
+          //understanding breast cancer section
+          <View style={styles.whiteOverlay}>
+            <View style={styles.background}>
+              <View style={styles.titleContainer}>
+                <ThemedText style={styles.understandingText}>
+                  Understanding
+                </ThemedText>
+                <ThemedText style={styles.bcText}>
+                  Breast Cancer
+                </ThemedText>
+              </View>
+              <View style={styles.grayLine} />
+              <ThemedText style={styles.paragraphTextTitle}>
+                What is Breast Cancer?
+              </ThemedText>
+              <ThemedText style={styles.paragraphText}>
+                Cancer: the development of abnormal cells that divide uncontrollably
+                and possess the ability to infiltrate and demolish normal body tissue.
+                {"\n\n"}In the body, there are trillions of cells that grow and divide to
+                help the body function properly. Cells die when they become old or damaged,
+                and new cells replace them.
+                {"\n\n"}Breast Cancer is when cancer forms in the breast. This disease can
+                occur in both women and men, but is far more prevalent in women.
+              </ThemedText>
+              <View style={styles.statContainer}>
+                <ThemedText style={styles.statTextBold}>2nd</ThemedText>
+                <ThemedText style={styles.statText}>
+                  most common cancer in women
+                </ThemedText>
+                <ThemedText style={styles.statTextBold}>1 out of 8</ThemedText>
+                <ThemedText style={styles.statText}>
+                  women will develop invasive breast cancer over the course of her
+                  lifetime
+                </ThemedText>
+              </View>
+              <StepIndicators totalSteps={totalSteps} currentStep={step} />
+              <LearnMoreTextContainer />
+            </View>
+          </View>
+        )}
+
+        {step === 1 && (
+          //purpose of a self-exam section
+          <View style={styles.whiteOverlay}>
+            <ThemedText style={[globalStyles.titleText, styles.titleText]}>
+              The Purpose of
+            </ThemedText>
+            <ThemedText
+              style={[
+                globalStyles.titleTextDarkPink,
+                styles.highlightedTitleText,
+              ]}
+            >
+              A Self-Exam
+            </ThemedText>
+            <View style={styles.grayLine} />
+            <ThemedText style={styles.noticeTitle}>Notice:</ThemedText>
+            <ThemedText style={styles.noticeText}>
+              A self-exam is not a substitute for annual breast cancer screenings.
+              <Text style={styles.boldText}> Mammograms are essential.</Text>
+            </ThemedText>
+            <ThemedText style={styles.noticeText2}>
+              The most recent American Cancer Society guidelines recommend women
+              ages <Text style={styles.highlightText}>40 to 44</Text> should consider
+              annual breast cancer screenings with mammograms. Women ages{" "}
+              <Text style={styles.highlightText}>45 to 54</Text> should get mammograms
+              every year. Women <Text style={styles.highlightText}>55 and older</Text>{" "}
+              should switch to mammograms every 2 years or can continue yearly screening.
+            </ThemedText>
+            <ThemedText style={styles.infoBoldText}>
+              Dr. Lopa’s self-exams are effective precautionary and preventative measures
+              for detecting symptoms of breast cancer.
+            </ThemedText>
+            <ThemedText style={styles.infoText}>
+              Being familiar with how our breasts look and feel can help us notice
+              symptoms such as lumps, pain, or changes in size that may be of concern
+              and respond to them.
+            </ThemedText>
+            <View style={styles.grayLine} />
+            <View style={styles.quotesContainer}>
+              <ThemedText style={styles.quoteText}>
+                “The difference is, this could save your life.”
+              </ThemedText>
+              <ThemedText style={styles.quoteText}>
+                “Mammograms are important, self-exams are how you are going to find
+                cancer early and save your life.”
+              </ThemedText>
+              <ThemedText style={styles.quoteText}>
+                “Early detection is the key.”
+              </ThemedText>
+            </View>
+            <StepIndicators totalSteps={totalSteps} currentStep={step} />
+            <LearnMoreTextContainer />
+          </View>
+        )}
+
+        {step === 2 && (
+          //breast cancer screening and techniques section
+          <View style={styles.whiteOverlay}>
+            <View style={styles.background}>
+              <View style={styles.titleContainer}>
+                <ThemedText style={styles.understandingText}>
+                  Breast Cancer
+                </ThemedText>
+                <ThemedText style={styles.bcText}>
+                  Screening & Techniques
+                </ThemedText>
+              </View>
+              <View style={styles.grayLine} />
+              <ThemedText style={styles.paragraphText}>
+                Breast cancer originates in one or both breasts. Early detection through regular screening is essential because it enables treatment at a stage when the cancer is most manageable, often resulting in less aggressive interventions and improved survival rates.
+                {"\n\n"}
+                Screening Methods:
+                {"\n\n"}• <ThemedText style={styles.boldText}>Breast Self-Examination:</ThemedText> It is advisable to begin self-exams between the ages of 17 and 18. Becoming familiar with the normal appearance and feel of your breast tissue can help you identify any changes or abnormalities promptly.
+                {"\n\n"}• <ThemedText style={styles.boldText}>Clinical Breast Examination:</ThemedText> This involves a physical examination of the breasts performed by a qualified healthcare provider.
+                {"\n\n"}• <ThemedText style={styles.boldText}>Mammography:</ThemedText> Women aged 40 and older should undergo annual mammograms. However, it is important to note that mammograms may miss approximately 50% of cancers in women with dense breast tissue.
+                {"\n\n"}• <ThemedText style={styles.boldText}>Advanced Imaging for Dense Breasts:</ThemedText> For women with dense breasts, additional screening methods such as 3-D mammography (tomosynthesis), breast MRI, breast ultrasound, or molecular breast imaging (MBI) may be recommended.
+                {"\n\n"}• <ThemedText style={styles.boldText}>High-Risk Individuals:</ThemedText> Those identified as having a high risk for breast cancer should consider initiating screening earlier and undergoing more frequent evaluations.
+              </ThemedText>
+              <StepIndicators totalSteps={totalSteps} currentStep={step} />
+              <LearnMoreTextContainer />
+            </View>
+          </View>
+        )}
+
+        {step === 3 && (
+          //additional resources and contact information section
+          <View style={styles.whiteOverlay}>
+            <View style={styles.background}>
+              <View style={styles.titleContainer}>
+                <ThemedText style={styles.understandingText}>
+                  Additional Resources &
+                </ThemedText>
+                <ThemedText style={styles.bcText}>
+                  Contact Information
+                </ThemedText>
+              </View>
+              <View style={styles.grayLine} />
+              <ThemedText style={styles.paragraphText}>
+                If you experience any symptoms or require further testing, please consult your healthcare provider promptly.
+                {"\n\n"}For additional support, you can download the "One-stop Lifesaving Early Detection Cards" for other prevalent cancers by visiting:
+                {"\n"}
+                <ExternalLink href="https://www.breastcancerhub.org/educational-cards" asChild>
+                  <TouchableOpacity>
+                    <ThemedText style={styles.linkText}>https://www.breastcancerhub.org/educational-cards</ThemedText>
+                  </TouchableOpacity>
+                </ExternalLink>
+                {"\n\n"}To learn more about our grassroots, sustainable solutions that are saving lives globally, please visit:
+                {"\n"}
+                <ExternalLink href="https://www.breastcancerhub.org" asChild>
+                  <TouchableOpacity>
+                    <ThemedText style={styles.linkText}>https://www.breastcancerhub.org</ThemedText>
+                  </TouchableOpacity>
+                </ExternalLink>
+                {"\n\n"}If you have any questions, please contact Dr. Lopamudra Das Roy, Founder-President of Breast Cancer Hub, at lopa@breastcancerhub.org.
+              </ThemedText>
+              <StepIndicators totalSteps={totalSteps} currentStep={step} />
+              <LearnMoreTextContainer />
+            </View>
+          </View>
+        )}
+
+
+        {/*navigation buttons*/}
+        <ThemedView style={styles.buttonContainer}>
+          {step > 0 && (
+            <TouchableOpacity style={globalStyles.buttonBack} onPress={handleBack}>
+              <ThemedText style={globalStyles.buttonTextBack}>Back</ThemedText>
+            </TouchableOpacity>
+          )}
+          
+          {/*conditional for when step = 0 (makes it so the next button stays to the right when the back button isn't present)*/}
+          {step === 0 ? (
+            <ThemedView style={styles.buttonStepZeroContainer}>
+              <TouchableOpacity style={globalStyles.buttonNext} onPress={handleNext}>
+                <ThemedText style={globalStyles.buttonTextNext}>Next</ThemedText>
+              </TouchableOpacity>
+            </ThemedView>
+          ) : (
+            <ThemedView style={styles.buttonBackNextContainer}>
+              {step < totalSteps - 1 ? (
+                <TouchableOpacity style={globalStyles.buttonNext} onPress={handleNext}>
+                  <ThemedText style={globalStyles.buttonTextNext}>Next</ThemedText>
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity style={globalStyles.buttonNext} onPress={() => router.push("/askMenstruate")}>
+                  <ThemedText style={globalStyles.buttonTextNext}>Finish</ThemedText>
+                </TouchableOpacity>
+              )}
+            </ThemedView>
+          )}
+        </ThemedView>
+      </ScrollView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  stepOneNextContainer: {
+    justifyContent: "flex-end",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    paddingBottom: 20, 
+  },
+  whiteOverlay: {
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 17,
+    borderTopRightRadius: 17,
+    padding: 20,
+    marginBottom: 20,
+  },
+  background: {
     padding: 10,
-    gap: 8,
   },
-  bodyContainer: {
+  titleContainer: {
     flexDirection: "column",
-    height: "100%",
-    margin: 10,
+    alignItems: "flex-start",
   },
-  popText: {
-    margin: "auto",
+  understandingText: {
+    color: colors.black,
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingTop: 10,
   },
+  bcText: {
+    color: colors.darkPink,
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingTop: 10,
+  },
+  grayLine: {
+    height: 2,
+    backgroundColor: colors.lightestGray,
+    marginVertical: 10,
+  },
+  paragraphTextTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  paragraphText: {
+    fontSize: 16,
+    color: colors.black,
+    marginVertical: 10,
+    lineHeight: 24,
+  },
+  statContainer: {
+    backgroundColor: "#F9F9F9",
+    padding: 10,
+    borderLeftWidth: 4,
+    borderColor: colors.darkPink,
+    marginVertical: 15,
+  },
+  statTextBold: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.darkPink,
+  },
+  statText: {
+    fontSize: 16,
+    color: colors.black,
+    marginBottom: 10,
+  },
+  titleText: {
+    paddingTop: 25,
+  },
+  highlightedTitleText: {
+    marginBottom: 15,
+    paddingTop: 10,
+  },
+  noticeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.darkPink,
+    marginVertical: 10,
+  },
+  noticeText: {
+    fontSize: 16,
+    color: colors.black,
+    lineHeight: 24,
+  },
+  noticeText2: {
+    fontSize: 15,
+    color: colors.darkGray,
+    lineHeight: 24,
+    fontStyle: "italic",
+    marginTop: 20,
+  },
+  highlightText: {
+    color: colors.darkPink,
+    fontWeight: "bold",
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+  infoText: {
+    fontSize: 16,
+    color: colors.black,
+    marginVertical: 10,
+    lineHeight: 24,
+  },
+  infoBoldText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: colors.black,
+    marginVertical: 15,
+    lineHeight: 24,
+    marginTop: 20,
+  },
+  quotesContainer: {
+    marginVertical: 20,
+  },
+  quoteText: {
+    fontSize: 16,
+    fontStyle: "italic",
+    color: colors.darkPink,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  buttonBackNextContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end", 
+    width: "90.3%", 
+    paddingHorizontal: 10, 
+    marginRight: 30
+  },
+  buttonStepZeroContainer: {
+    flexDirection: "row", 
+    justifyContent: "flex-end", 
+    width: "100%", 
+    paddingHorizontal: 10, 
+    marginRight: 30
+  },
+  linkText: {
+    color: "68C4FF"
+  }
 });
---
+++ breast-cancer-hub/components/StepIndicators.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+interface StepIndicatorsProps {
+  totalSteps: number;
+  currentStep: number;
+}
+
+const StepIndicators: React.FC<StepIndicatorsProps> = ({ totalSteps, currentStep }) => {
+    return (
+      <View style={styles.indicatorContainer}>
+        {Array.from({ length: totalSteps }).map((_, index) => {
+          //adjusts the comparison so the first step is considered at index 0
+          const iconName = index == currentStep ? "circle" : "circle-o";
+          return (
+            <FontAwesome
+              key={index}
+              name={iconName}
+              size={24}
+              color="#E93C92" 
+              style={styles.indicator}
+            />
+          );
+        })}
+      </View>
+    );
+  };
+
+const styles = StyleSheet.create({
+  indicatorContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  indicator: {
+    marginHorizontal: 5,
+  },
+});
+
+export default StepIndicators;
