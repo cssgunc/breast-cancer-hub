d4436b143a934b5d628fe678de2bfe026b0af6f1
DieDyeDai <mzdai@ad.unc.edu>
2025-04-09 19:57:11 -0400

    Add line to fix bug on mobile

--- breast-cancer-hub/app/_layout.tsx
+++ breast-cancer-hub/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-
+window.addEventListener = (x: any) => x;
 export default function TabLayout() {
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
 }
