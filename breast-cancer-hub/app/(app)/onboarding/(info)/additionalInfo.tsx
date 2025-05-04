0fa38491a9be48ee18a1b4b16f6d865b57a2e30b
DieDyeDai <mzdai@ad.unc.edu>
2025-05-04 13:19:47 -0400

    Fix link spacing in onboarding

--- breast-cancer-hub/app/(app)/onboarding/(info)/additionalInfo.tsx
+++ breast-cancer-hub/app/(app)/onboarding/(info)/additionalInfo.tsx
@@ -1,13 +1,20 @@
 import { ExternalLink } from "@/components/navigation/ExternalLink";
 import { ThemedText } from "@/components/style/ThemedText";
 import { ThemedView } from "@/components/style/ThemedView";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { onboardingStyles } from "..";
 import { useColors } from "@/components/style/ColorContext";
 import { LearnMoreTextContainer } from "@/components/LearnMoreText";
 
 export default function AdditionalInfo() {
   const { colors, globalStyles } = useColors();
+  const styles = StyleSheet.create({
+    link: {
+      marginTop: -24,
+      marginBottom: -36,
+      lineHeight: 24,
+    }
+  })
   return (
     <>
       <ThemedView style={onboardingStyles.titleContainer}>
@@ -24,28 +31,33 @@ export default function AdditionalInfo() {
           {"\n\n"}For additional support, you can download the "One-stop
           Lifesaving Early Detection Cards" for other prevalent cancers by
           visiting:
-          {"\n"}
-          <ExternalLink
-            href="https://www.breastcancerhub.org/educational-cards"
-            asChild
-          >
-            <TouchableOpacity>
-              <ThemedText type="link">
-                https://www.breastcancerhub.org/educational-cards
-              </ThemedText>
-            </TouchableOpacity>
-          </ExternalLink>
-          {"\n\n"}To learn more about our grassroots, sustainable solutions that
+        </ThemedText>
+        <ExternalLink
+          href="https://www.breastcancerhub.org/educational-cards"
+          asChild
+        >
+          <TouchableOpacity>
+            <ThemedText type="link" style={styles.link}>
+              https://www.breastcancerhub.org/educational-cards
+            </ThemedText>
+          </TouchableOpacity>
+        </ExternalLink>
+        <ThemedText>
+          {"\n"}To learn more about our grassroots, sustainable solutions that
           are saving lives globally, please visit:
-          {"\n"}
-          <ExternalLink href="https://www.breastcancerhub.org" asChild>
-            <TouchableOpacity>
-              <ThemedText type="link">
-                https://www.breastcancerhub.org
-              </ThemedText>
-            </TouchableOpacity>
-          </ExternalLink>
-          {"\n\n"}If you have any questions, please contact Dr. Lopamudra Das
+        </ThemedText>
+        <ExternalLink
+          href="https://www.breastcancerhub.org"
+          asChild
+        >
+          <TouchableOpacity>
+            <ThemedText type="link" style={styles.link}>
+              https://www.breastcancerhub.org
+            </ThemedText>
+          </TouchableOpacity>
+        </ExternalLink>
+        <ThemedText>
+          If you have any questions, please contact Dr. Lopamudra Das
           Roy, Founder-President of Breast Cancer Hub, at
           lopa@breastcancerhub.org.
         </ThemedText>
