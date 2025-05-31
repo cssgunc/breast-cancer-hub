8aaefef0d4793da4f32ff0f6de63634f379ee2ad
Amy Xu <knightofbirds@gmail.com>
2025-05-30 21:05:16 -0400

    Add logo and message to welcome screen

--- breast-cancer-hub/app/welcome.tsx
+++ breast-cancer-hub/app/welcome.tsx
@@ -1,16 +1,31 @@
-import { KeyboardAvoidingView, Platform, TextInput, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
 import { ThemedView } from "@/components/style/ThemedView";
 import { ThemedText } from "@/components/style/ThemedText";
 import { useRouter } from "expo-router";
 import { getSetting, saveSetting } from "@/hooks/useSettings";
 import { useColors } from "@/components/style/ColorContext";
 import { MaterialIcons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemedButton from "@/components/ThemedButton";
 export default function WelcomePage() {
   const router = useRouter();
   const [name, setName] = useState<string>("");
   const { globalStyles } = useColors();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getSetting("name");
+      setName(value);
+    };
+
+    init();
+  }, []);
 
   const handleSubmit = async () => {
     saveSetting("userId", "local");
@@ -41,35 +56,49 @@ export default function WelcomePage() {
               WELCOME
             </ThemedText>
             <ThemedText type="title">to the</ThemedText>
-            <ThemedText type="title" colored>
-              Breast Cancer Hub
-            </ThemedText>
+            <Image
+              source={require("@/assets/images/BCH-Logo-Stacked-CMYK.png")}
+              style={{ width: 250, height: 150 }}
+              resizeMethod="auto"
+              resizeMode="contain"
+            />
             <ThemedText type="title">Self-Exam App!</ThemedText>
           </ThemedView>
           <ThemedView style={{ gap: 20, padding: 20 }}>
             <ThemedText>
-              Breast Cancer Hub (BCH)'s "Know Your Breasts" App serves as a
-              vital monthly reminder for individuals of All Genders to perform
-              Breast Self-Exams (BSE). Early detection saves lives!
+              Breast Cancer Hub (BCH)'s "
+              <ThemedText colored bold>
+                Know Your Breasts
+              </ThemedText>
+              " App serves as a vital monthly reminder for individuals of{" "}
+              <ThemedText colored bold>
+                All Genders{" "}
+              </ThemedText>
+              to perform Breast Self-Exams (BSE).{" "}
+              <ThemedText colored bold>
+                Early Detection Saves Lives!
+              </ThemedText>
             </ThemedText>
           </ThemedView>
 
-          <View style={globalStyles.loginInputContainer}>
-            <TextInput
-              style={globalStyles.loginInput}
-              placeholder="Name (optional, can be changed at any time)"
-              placeholderTextColor="gray"
-              value={name}
-              onChangeText={setName}
-              autoCapitalize="none"
-            />
-            <MaterialIcons
-              name="person"
-              size={24}
-              color="gray"
-              style={globalStyles.loginIcon}
-            />
-          </View>
+          {!name && (
+            <View style={globalStyles.loginInputContainer}>
+              <TextInput
+                style={globalStyles.loginInput}
+                placeholder="Name (optional, can be changed at any time)"
+                placeholderTextColor="gray"
+                value={name}
+                onChangeText={setName}
+                autoCapitalize="none"
+              />
+              <MaterialIcons
+                name="person"
+                size={24}
+                color="gray"
+                style={globalStyles.loginIcon}
+              />
+            </View>
+          )}
           <ThemedView style={globalStyles.loginInputsContainer}>
             <ThemedButton
               onPress={handleSubmit}
